acf284afa95b2d285c451818580a55d25efaa00f
Lauren Eggers <neyabenz@gmail.com>
2018-09-14 15:30:33 -0400

    store reviews in IDB

--- js/idb.js
+++ js/idb.js
@@ -4,11 +4,10 @@ function storeRestaurants() {
 	//retrieve json
 	fetch(`${DBHelper.DATABASE_URL}`, {method: 'GET'})
 		.then(response => {
-			console.log('1 Got the json for IDBIATCH', response);
+			//take response and convert to json
 			return response.json();
-			//place json
 		}).then (restaurants => {
-			console.log('Begin placement to IDBIATCH', restaurants);
+			//open transaction to write in restaurants
 			dbPromise.then(function (db) {
 				if (!db) return;
 				let tx = db.transaction('restaurants', 'readwrite');
@@ -16,13 +15,38 @@ function storeRestaurants() {
 				//place each restaurant in DB
 				restaurants.forEach(restaurant => {
 					store.put(restaurant);
-					console.log('Successfully cached each restaurant in IDBiatch', restaurant);
 				});
 				return tx.complete;
 			});
 		});
 }
+
+function storeReviews() {
+	let dbPromise = DBHelper.openDB();
+	//retrieve json
+	fetch(`${DBHelper.REVIEW_URL}`, {method: 'GET'})
+		.then(response => {
+			//get response and return as json
+			return response.json();
+			//place json
+		}).then (reviews => {
+			//take reviews, use database transaction to write reviews in db
+			dbPromise.then(function (db) {
+				if (!db) return;
+				let tx = db.transaction('reviews', 'readwrite');
+				let store = tx.objectStore('reviews');
+				//place each restaurant in DB
+				reviews.forEach(review => {
+					store.put(review);
+					//put into db
+				});
+				return tx.complete;
+			});
+		});
+}
+
 //steps in order
 DBHelper.openDB()
 	.then(storeRestaurants())
+	.then(storeReviews())
 	.catch((err) => console.log(`IDB is unhappy and failed with ${err}`));
