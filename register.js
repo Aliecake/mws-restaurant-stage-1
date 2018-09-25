ab1f20d90f2f430b7f8748bb2ad3fa17f00563ce
Lauren Eggers <neyabenz@gmail.com>
2018-09-25 18:18:15 -0400

    online again

--- register.js
+++ register.js
@@ -1,4 +1,3 @@
-// temporarily took SW offline to work on Project 3
 // app for service worker help sources include https://developers.google.com/web/fundamentals/primers/service-workers/
 
 // if able, register service worker and success message
@@ -6,7 +5,7 @@ if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker.register('/sw.js', {scope: './'}).then((registration) => {
 			// successful
-			console.log('Service Worker -Registered- With scope: ', registration.scope);
+			//console.log('Service Worker -Registered- With scope: ', registration.scope);
 		}, (err) => {
 			// registration failed
 			console.log('Service Worker -Failed to Register-', err);
